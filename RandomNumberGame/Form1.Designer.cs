4b85d1103da208ff8617394d89c5b922d0c35df8
TheGingerNinjaC <vanderbergchane@gmail.com>
2018-09-02 15:03:04 +0200

    Initial commit

---
+++ RandomNumberGame/Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace RandomNumberGame
+{
+    partial class numberGameForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.descriptionLabel = new System.Windows.Forms.Label();
+            this.guessButton = new System.Windows.Forms.Button();
+            this.guessTextBox = new System.Windows.Forms.TextBox();
+            this.outputListBox = new System.Windows.Forms.ListBox();
+            this.newGameButton = new System.Windows.Forms.Button();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // descriptionLabel
+            // 
+            this.descriptionLabel.AutoSize = true;
+            this.descriptionLabel.Font = new System.Drawing.Font("Showcard Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.descriptionLabel.Location = new System.Drawing.Point(12, 9);
+            this.descriptionLabel.Name = "descriptionLabel";
+            this.descriptionLabel.Size = new System.Drawing.Size(319, 51);
+            this.descriptionLabel.TabIndex = 0;
+            this.descriptionLabel.Text = "WELCOME TO THE GUESSING GAME!\r\n\r\nEnter a number between 0 and 100 to start.";
+            this.descriptionLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // guessButton
+            // 
+            this.guessButton.Location = new System.Drawing.Point(188, 78);
+            this.guessButton.Name = "guessButton";
+            this.guessButton.Size = new System.Drawing.Size(75, 23);
+            this.guessButton.TabIndex = 1;
+            this.guessButton.Text = "Guess";
+            this.guessButton.UseVisualStyleBackColor = true;
+            this.guessButton.Click += new System.EventHandler(this.guessButton_Click);
+            // 
+            // guessTextBox
+            // 
+            this.guessTextBox.Location = new System.Drawing.Point(73, 80);
+            this.guessTextBox.Name = "guessTextBox";
+            this.guessTextBox.Size = new System.Drawing.Size(100, 20);
+            this.guessTextBox.TabIndex = 0;
+            // 
+            // outputListBox
+            // 
+            this.outputListBox.FormattingEnabled = true;
+            this.outputListBox.Location = new System.Drawing.Point(15, 120);
+            this.outputListBox.Name = "outputListBox";
+            this.outputListBox.Size = new System.Drawing.Size(316, 160);
+            this.outputListBox.TabIndex = 2;
+            // 
+            // newGameButton
+            // 
+            this.newGameButton.Location = new System.Drawing.Point(73, 297);
+            this.newGameButton.Name = "newGameButton";
+            this.newGameButton.Size = new System.Drawing.Size(75, 32);
+            this.newGameButton.TabIndex = 3;
+            this.newGameButton.Text = "New Game";
+            this.newGameButton.UseVisualStyleBackColor = true;
+            this.newGameButton.Click += new System.EventHandler(this.newGameButton_Click);
+            // 
+            // exitButton
+            // 
+            this.exitButton.Location = new System.Drawing.Point(188, 297);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(75, 32);
+            this.exitButton.TabIndex = 4;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // numberGameForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(345, 343);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.newGameButton);
+            this.Controls.Add(this.outputListBox);
+            this.Controls.Add(this.guessTextBox);
+            this.Controls.Add(this.guessButton);
+            this.Controls.Add(this.descriptionLabel);
+            this.Name = "numberGameForm";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.numberGameForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label descriptionLabel;
+        private System.Windows.Forms.Button guessButton;
+        private System.Windows.Forms.TextBox guessTextBox;
+        private System.Windows.Forms.ListBox outputListBox;
+        private System.Windows.Forms.Button newGameButton;
+        private System.Windows.Forms.Button exitButton;
+    }
+}
+
---
+++ RandomNumberGame/Form1.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RandomNumberGame
+{
+    public partial class numberGameForm : Form
+    {
+
+        public numberGameForm()
+        {
+            InitializeComponent();
+        }
+
+        // Create variable to store random
+        int number;
+
+        // Variables to store user guess and count
+        int guess = 0;
+        int count = 0;
+              
+              
+        private void numberGameForm_Load(object sender, EventArgs e)
+        {
+
+            // Create random object
+            Random rand = new Random();
+
+            // Get a random integer and assign it to number
+            number = rand.Next(100);
+
+        }
+
+        private void guessButton_Click(object sender, EventArgs e)
+        {
+
+            // Count increases with each guess
+            count++;
+
+            // Read user input
+            guess = int.Parse(guessTextBox.Text);
+
+            // Determine if guess is smaller than random number
+            if (guess < number)
+            {
+                outputListBox.Items.Add("Too low! Try again");
+            }
+
+            // Determine if guess is larger than random number
+            if (guess > number)
+            {
+                outputListBox.Items.Add("Too high! Try again");
+            }
+
+            // Determine if guess is equal to random number
+            if (guess == number)
+            {
+                // Output to listbox
+                outputListBox.Items.Add("Well done! You've guessed correctly!"
+                    + " Amount of guesses: " + count + ".");
+
+                // Set a new random number
+                Random rand = new Random();
+                number = rand.Next(100);
+
+                // Clear count
+                count = 0;
+
+                // Alert player that new round has started
+                outputListBox.Items.Add("New round! Enter a number between 1 and 100");
+
+                // Clear textbox and set focus
+                guessTextBox.Text = "";
+                guessTextBox.Focus();
+
+            }
+
+          
+        }
+
+        private void newGameButton_Click(object sender, EventArgs e)
+        {
+            // Clear the item box and text box
+            outputListBox.Items.Clear();
+            guessTextBox.Text = "";
+
+            // Set focus to text box
+            guessTextBox.Focus();
+
+            // Set a new random number
+            Random rand = new Random();
+            number = rand.Next(100);
+
+            // Clear count
+            count = 0;
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
